034128296380c772f8292fb705066b7083a86de1
Thet Khine <mrthetkhine@gmail.com>
2020-01-26 16:36:37 +0630

    Update

--- src/app/component/movie/movie.component.ts
+++ src/app/component/movie/movie.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
+  name:string = 'Avatar';
+  year: number = 2020;
   constructor() { }
 
   ngOnInit() {
