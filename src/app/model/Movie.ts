752e61b333ca836dd7f8467155441dd2860bac24
Thet Khine <mrthetkhine@gmail.com>
2020-01-26 16:31:43 +0630

    Update

---
+++ src/app/model/Movie.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by thetkhine on 1/26/20.
+ */
+export class Movie
+{
+  title : string;
+  year : number;
+
+  constructor(title : string, year :number)
+  {
+    this.title = title;
+    this.year = year;
+  }
+}
